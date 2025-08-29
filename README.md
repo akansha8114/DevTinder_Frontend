166288e172cf2c792b845eb7d69396a47bac5468
akansha8114 <akanshavgk2004@gmail.com>
2025-08-29 20:35:53 +0530

    Redirect to login of not Authenticated

--- README.md
+++ README.md
@@ -11,11 +11,16 @@
 -Install axios and cors -> add middleware to with backend: origin , credentials:true
 - whenever u are makin aAPi call always pass axios => with {credentials:true}
 -CORs error , cross oriign domain.
+
 -install redux toolkit => create a configureStore=> add a provider in app.js => add reducer to the store.
 -Add redux toolkit in chrome
 -login and see if data is coming properly in redux store
 -NavBar should update as soon as we logged in 
 -Refactor our code to add constants file and create a components folder.
+-You should not be access other routes without login
+-if token is not present , redirect to the login page 
+-now build Logut and Profile page
+
 
 
 
--- src/Body.jsx
+++ src/Body.jsx
@@ -1,9 +1,39 @@
 import React from 'react'
 import NavBar from './NavBar'
+import { useNavigate } from 'react-router-dom'
+import  Base_URL  from './utils/constants';
+import axios from 'axios'
+import { useSelector } from 'react-redux'
+import { useDispatch } from 'react-redux'
+import { addUser } from './utils/userSlice'
+import { useEffect } from 'react'
 import { Outlet } from 'react-router-dom'
 import Footer from './Footer'
 
 const Body = () => {
+  const dispatch  = useDispatch();
+  const navigate = useNavigate();
+  const userData = useSelector((store) => store.user);
+  const fetchUser = async() =>{
+    if(userData) return;
+    try{
+      const res = await axios.get(Base_URL + "/profile/view" , {
+        withCredentials:true,
+      });
+      dispatch(addUser(res.data));
+    }catch(err){
+      if(err.status ===401){
+        navigate("/login");
+      }
+      else{
+        console.log(err);
+      }
+    }
+  }
+  useEffect(()=>{
+    fetchUser();
+  },[]);
+
   return (
     <div>
         <NavBar/>
